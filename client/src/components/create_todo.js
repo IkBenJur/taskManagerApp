5969943b6c881a4c45f280b902e5ea5e4cbede91
IkBenJur <jurspammer@gmail.com>
2023-09-19 23:52:08 +0200

    Feat: created component files for routes

---
+++ client/src/components/create_todo.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function CreateTodo() {
+    const [todoForm, setTodoForm] = useState({
+        title: "",
+        description: "",
+    });
+    const navigate = useNavigate();
+
+    function updateTodoForm(value) {
+        return setTodoForm((prev) => {
+            return { ...prev, ...value };
+        });
+    };
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        const newTodo = { ...todoForm };
+
+        await fetch("http://localhost:5000/todos/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTodo)
+        })
+        .catch( err => {
+            window.alert(err);
+            return;
+        });
+
+        setTodoForm({
+            title: "",
+            description: "",
+        });
+        navigate("/");
+    }
+
+    return (
+        <div>
+            Hello Create
+        </div>
+    )
+}
