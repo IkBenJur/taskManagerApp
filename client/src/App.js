966d8fe8c1c4e2d66e462462ba9cba7220523110
IkBenJur <jurspammer@gmail.com>
2023-09-19 23:51:32 +0200

    Created base for react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CreateTodo from "./components/create_todo";
+
+const App = () => {
+ return (
+   <div>
+    <CreateTodo />
+    Hello
+   </div>
+ );
+};
+ 
+export default App;
