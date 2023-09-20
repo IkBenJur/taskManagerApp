abd98f3bbf9b5180f1450270d45e238d246889f7
IkBenJur <jurspammer@gmail.com>
2023-09-20 13:54:20 +0200

    Feat: update todo via form

--- client/src/components/edit_todo.js
+++ client/src/components/edit_todo.js
@@ -35,11 +35,55 @@ export default function EditTodo() {
 
         return;
       }, [params.id, navigate]);
-     
+    
+    function updateTodoForm(newTodoForm) {
+        return setTodoForm((prev) => {
+            return { ...prev, ...newTodoForm }
+        });
+    };
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        const updatedTodo = { ...todoForm };
+
+        const id = params.id.toString();
+        const response = await fetch(`http://localhost:5000/todos/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedTodo),
+        });
+
+        console.log(await response.json())
+
+        navigate('/');
+    }
 
     return (
         <div>
-            Hello {params.id.toString()}
+            <h1>Update todo</h1>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="title">Title</label>
+                <input 
+                    type="text"
+                    id="title"
+                    value={todoForm.title}
+                    onChange={(e) => updateTodoForm({ title: e.target.value })}
+                />
+                <label htmlFor="description">Description</label>
+                <textarea 
+                    type="text"
+                    id="description"
+                    value={todoForm.description}
+                    onChange={(e) => updateTodoForm({ description: e.target.value })}
+                />
+                <input 
+                    type="submit"
+                    value="Update Todo"
+                />
+            </form>
         </div>
     )
 }
