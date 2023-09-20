fd8492e56871348b4f746ad1dfa4f84e1ab3556a
IkBenJur <jurspammer@gmail.com>
2023-09-20 13:31:07 +0200

    Feat: loads correct todo to edit

--- client/src/components/edit_todo.js
+++ client/src/components/edit_todo.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router";
 
 export default function EditTodo() {
-    const [form, setForm] = useState({
-        name: "",
-        position: "",
-        level: "",
-        records: [],
+    const [todoForm, setTodoForm] = useState({
+        title: "",
+        description: ""
       });
+
       const params = useParams();
+      const navigate = useNavigate();
+
+      useEffect(() => {
+        async function fetchTodo () {
+            const todoId = params.id.toString()
+            const response = await fetch(`http://localhost:5000/todos/${todoId}`)
+
+            if (!response.ok) {
+                const error = `An error occured: ${response.statusText}`;
+                window.alert(error);
+                return;
+            };
+
+            const todo = await response.json();
+            if (!todo) {
+                window.alert(`Todo with ID ${todoId} not found`);
+                navigate('/');
+                return;
+            };
+
+            setTodoForm(todo);
+        };
+
+        fetchTodo();
+
+        return;
+      }, [params.id, navigate]);
      
 
     return (
